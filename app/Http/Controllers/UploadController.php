4b16039a0a9e0d666250a6e15e418b5a0b927aa1
fitrmeyy <e41231686@student.polije.ac.id>
2025-03-17 10:35:11 +0700

    Initial commit

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+
+class UploadController extends Controller {
+    use ValidatesRequests;
+
+    public function upload(){
+        return view('upload');
+    }
+
+    public function proses_upload(Request $request){
+        $this->validate($request, [
+            'file' =>'required',
+            'keterangan' => 'required',
+        ]);
+        // menyimpan data file yang diupload ke variabel $file
+        $file = $request->file('file');
+        // nama file
+        echo 'File Name: '.$file->getClientOriginalName().'<br>';
+        // ekstensi file
+        echo 'File Extension: '.$file->getClientOriginalExtension().'<br>';
+        // real path
+        echo 'File Real Path: '.$file->getRealPath().'<br>';
+        // ukuran file
+        echo 'File Size: '.$file->getSize().'<br>';
+        // tipe mime
+        echo 'File Mime Type: '.$file->getMimeType().'<br>';
+        $tujuan_upload = 'data_file';
+        // upload file
+        $file->move($tujuan_upload, $file->getClientOriginalName());
+    }
+
+    public function dropzone()
+    {
+        return view('dropzone');
+    }
+
+    public function dropzone_store(Request $request)
+    {
+        $image = $request->file('file');
+
+        $imageName = time().'.'.$image->extension();
+        $image->move(public_path('img/dropzone'), $imageName);
+        return response()->json(['success' => $imageName]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\UploadController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/upload', [UploadController::class, 'upload'])->name('upload');
+Route::post('/upload/proses', [UploadController::class, 'proses_upload'])->name('upload.proses');
+Route::post('/upload/resize', [UploadController::class, 'resize_upload'])->name('upload.resize');
+Route::get('/dropzone', [UploadController::class, 'dropzone'])->name('dropzone');
+Route::post('/dropzone/upload', [UploadController::class, 'dropzone_store'])->name('dropzone_store');
